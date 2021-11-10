c21f6f902d8d80d75cbec8d615e3c2330514e63a
Reuven Lerner <reuven@lerner.co.il>
2021-11-10 11:39:11 +0200

    gitautosync update 140

---
+++ flycheck_icecream.py
@@ -0,0 +1,19 @@
+class Scoop:
+    def __init__(self, flavor):
+        self.flavor = flavor
+
+
+class Bowl:
+    def __init__(self):
+        self.scoops = []
+
+    def add_scoops(self, *args):
+        for one_scoop in args:
+
+            self.scoops.append(one_scoop)
+
+    def flavors(self):
+        output = []
+        for one_scoop in self.scoops:
+            output.append(one_scoop.flavor)
+        return output
