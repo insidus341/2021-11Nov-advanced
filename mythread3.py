1e6509b5ac1624deee17b01f363054ff0a706320
Reuven Lerner <reuven@lerner.co.il>
2021-11-11 16:38:52 +0200

    gitautosync update 183

--- mythread3.py
+++ mythread3.py
@@ -8,7 +8,7 @@ import random
 
 def hello(n):
     time.sleep(random.randint(0, 3))
-    print(f'{n} Hello!')        # add this to the end of the queue
+    return f'{n} Hello!'
 
 
 with ThreadPoolExecutor(max_workers=4) as executor:
