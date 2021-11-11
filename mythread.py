79bf6c60e7457beb0da7ea70783fc6e1df39e61a
Reuven Lerner <reuven@lerner.co.il>
2021-11-11 16:18:38 +0200

    gitautosync update 141

--- mythread.py
+++ mythread.py
@@ -11,5 +11,6 @@ all_threads = []
 for i in range(10):
     # create a new Thread object
     t = threading.Thread(target=hello, args=(i,))
+
     t.start()   # run the thread's function in a new thread
     all_threads.append(t)
