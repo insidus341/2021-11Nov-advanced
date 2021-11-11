fac529d2088cec9c7af9acfeec4f39fe44e47605
Reuven Lerner <reuven@lerner.co.il>
2021-11-11 16:34:20 +0200

    gitautosync update 171

--- mythread2.py
+++ mythread2.py
@@ -7,21 +7,9 @@ import random
 import threading
 import queue
 
-q = queue.Queue()               # thread-safe data structure!
-l = threading.Lock()            # create a "mutex"
-
 
 def hello(n):
-    time.sleep(random.randint(0, 3))
-
-    # From here, treat it as atomic -- don't let multiple threads in this region simulteously
-
-    with l:
-        q.put(f'{n} Hello!')        # add this to the end of the queue
-        time.sleep(random.randint(0, 3))
-        q.put(f'{n} Goodbye!')      # add this to the end of the queue
-
-    # Above here, treat it as atomic -- don't let multiple threads in this region simulteously
+    q.put(f'{n} Hello!')        # add this to the end of the queue
 
 
 all_threads = []
