58e68cdf943ae9ea9febb54aa75c7224fb6aabd5
Reuven Lerner <reuven@lerner.co.il>
2021-11-11 16:27:16 +0200

    gitautosync update 155

---
+++ flycheck_mythread.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import time
+import random
+import threading
+import queue
+
+q = queue.Queue()   # thread-safe data structure!
+
+
+def hello(n):
+    time.sleep(random.randint(0, 3))
+    q.put(f'{n} Hello!')        # add this to the end of the queue
+
+
+all_threads = []
+for i in range(10):
+    # create a new Thread object
+    t = threading.Thread(target=hello, args=(i,))
+
+    t.start()   # run the thread's function in a new thread
+    all_threads.append(t)
+
+
+# make sure that all threads have finished running
+# before they get to this point
+
+# we can wait for a thread to finish with the "join" method
+for one_thread in all_threads:
+    one_thread.join()
+
+print('Done!')
+
+while not q.empty():
+    print(q.get())   # retrieve from the top of the queue
