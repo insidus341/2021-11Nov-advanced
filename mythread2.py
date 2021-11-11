e0d03cc0df6a45a7ed10730908e2533f246beda5
Reuven Lerner <reuven@lerner.co.il>
2021-11-11 16:36:24 +0200

    gitautosync update 176

--- mythread2.py
+++ mythread2.py
@@ -9,26 +9,12 @@ import threading
 
 def hello(n):
     time.sleep(random.randint(0, 3))
-    q.put(f'{n} Hello!')        # add this to the end of the queue
+    print(f'{n} Hello!')        # add this to the end of the queue
 
 
-all_threads = []
-for i in range(10):
-    # create a new Thread object
-    t = threading.Thread(target=hello, args=(i,))
+with ThreadPoolExecutor(max_workers=4) as executor:
+    for i in range(10):
+        executor.submit(hello, i)
 
-    t.start()   # run the thread's function in a new thread
-    all_threads.append(t)
-
-
-# make sure that all threads have finished running
-# before they get to this point
-
-# we can wait for a thread to finish with the "join" method
-for one_thread in all_threads:
-    one_thread.join()
 
 print('Done!')
-
-while not q.empty():
-    print(q.get())   # retrieve from the top of the queue
